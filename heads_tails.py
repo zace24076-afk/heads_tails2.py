3f8f6bbd3bd1486240723ed0d51813b10cbd81ac
zace24076-afk <zace.24076@obhs.school.nz>
2026-08-13 10:25:17 +1200

    Update heads_tails.py

--- heads_tails.py
+++ heads_tails.py
@@ -10,34 +10,67 @@
 import random
 
 #----------functions--------------------
+# Game play 
 def heads_tails():
     user_score = 0
     comp_score = 0
     
     while comp_score != 3 and user_score != 3:
         comp_guess = random.randint(0,1)
-        user_guess = int(input("Type 0 for Heads or 1 for Tails: "))
+        user_guess = int(input('Type 0 for Heads or 1 for Tails: '))
         
         if user_guess == comp_guess:
-            print("You won this round!")
+            print('You won this round!')
             user_score += 1
         else:
-            print("You lost this round!")
+            print('You lost this round!')
             comp_score += 1
 
     if user_score == 3:
-        print("Congrats {} you beat the computer!!!".format(first_name))
+        print('Congrats {} you beat the computer!!!'.format(first_name))
     else:
-        print("You lost, better luck next time {}!".format(first_name))
+        print('You lost, better luck next time {}!'.format(first_name))
 
+# Checking the name validation
+def force_name(first_name):
+    while (valid): # this means the loop doesn't end until the name is valid
+        name = input('What is your first name: ')
+        if(len(name) > min_len and len(name) < max_len ): #checking range
+            if(name.isalpha()):
+                print('Your name is valid')
+                break # this exits the loop
+            else:
+                print('Only use letters in your name')
+        else:
+            print('Your name is an invalid length, try again')
+    return first_name # sends the name back to the main routine
 
+def force_age(age):
+    while (valid):
+        age = int(input('What is your age: '))
+        if(age > min_age and age < max_age ): #checking range
+            if(age.isalpha()):
+                print('Age is valid')
+                break # this exits the loop
+            else:
+                print('Only use numbers in your name')
+        else:
+            print('You age is invalid')
+    return age # sends the name back to the main routine
 # -----------main_routine-----------------
-print("Hi, this is the heads and tails game")
-first_name = str(input("What is your name: "))
+print('Hi, this is the heads and tails game')
 
+valid = True
+min_len = 2
+max_len = 20
+first_name = force_name
+force_name(first_name)
+
+min_age = 12
+max_age = 19
+age = force_age
+force_age(age)
 
 age = int(input("Enter your age: "))
+
 heads_tails()
-#i love cufvvvekjfe
-#jerfkwjebfjkw
-#gklvnlv
